7cd30407e1d764dbf7c66b988d86e5b4e1eda9ea
naorh <Hili2018>
2019-06-08 13:34:52 +0300

    cache

--- src/main/java/com/naor/flightapp/service/CacheService.java
+++ src/main/java/com/naor/flightapp/service/CacheService.java
@@ -1,4 +1,58 @@
 package com.naor.flightapp.service;
 
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class CacheService {
+
+    private Map<Integer, TicketTTL> cache = new HashMap<>();
+
+    public void set(int ticketId, boolean res) {
+        cache.put(ticketId, new TicketTTL(res));
+    }
+
+    public Boolean get(int ticketId) {
+        TicketTTL ticketTTL = cache.get(ticketId);
+        if (ticketTTL == null) {
+            return null;
+        }
+        if (ticketTTL.getTtl() < System.currentTimeMillis()) {
+            return ticketTTL.getRes();
+        }
+        //expiration time passed
+        cache.remove(ticketId);
+        return null;
+    }
+
+    private class TicketTTL {
+        private boolean res;
+        private long ttl;
+
+        final int EXPIRATION_TTL = 1000 * 30;
+
+        public TicketTTL(boolean res) {
+            this.res = res;
+            this.ttl = System.currentTimeMillis() + EXPIRATION_TTL ;
+        }
+
+        public boolean getRes() {
+            return res;
+        }
+
+        public void setRes(boolean res) {
+            this.res = res;
+        }
+
+        public long getTtl() {
+            return ttl;
+        }
+
+        public void setTtl(long ttl) {
+            this.ttl = ttl;
+        }
+    }
 }
+
--- src/main/java/com/naor/flightapp/service/TicketsService.java
+++ src/main/java/com/naor/flightapp/service/TicketsService.java
@@ -7,15 +7,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TicketsService {
-    final TicketsDao ticketsDao;
+    private final TicketsDao ticketsDao;
+    private final CacheService cacheService;
 
     @Autowired
-    public TicketsService(TicketsDao ticketsDao) {
+    public TicketsService(TicketsDao ticketsDao, CacheService cacheService) {
         this.ticketsDao = ticketsDao;
+        this.cacheService = cacheService;
     }
 
     public boolean isTicketAvailable(int ticketId) {
+        Boolean cacheRes = cacheService.get(ticketId);
+        if (cacheRes != null) {
+            return cacheRes;
+        }
         final Ticket ticket = ticketsDao.get(ticketId);
-        return ticket != null && ticket.isAvailable();
+        boolean res = ticket != null && ticket.isAvailable();
+        cacheService.set(ticketId, res);
+        return res;
     }
 }
